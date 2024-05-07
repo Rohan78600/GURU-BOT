3a5513a3e4355c737c62ee5694563897dfc47a5b
Rohan78600 <megic0007@gmail.com>
2024-05-07 23:49:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.owner = [['917605902011', 'GURU', true], ['6281255369012', 'Ash', true], 
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
+global.mods = ['918768622684', '918768622684']
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
